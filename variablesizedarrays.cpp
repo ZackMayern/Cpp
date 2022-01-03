f5d140800bb17039348382aa67a24bf9e35aa5bd
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-01-03 13:31:43 +0530

    Introduction Challenges solved

---
+++ variablesizedarrays.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int n,q;
+    cin>>n>>q;
+    vector<vector<int>> a(n);
+    for(int i=0; i<n; i++){
+        int k;
+        cin>>k;
+        
+        a[i].resize(k);
+        for(int j=0; j<k; j++){
+            cin>>a[i][j];
+        }
+    }  
+    
+    //To run queries
+    for (int q_num = 0; q_num < q; q_num++){
+        int x,y;
+        cin>>x>>y;
+        cout<<a[x][y]<<endl;
+    }
+    return 0;
+}
